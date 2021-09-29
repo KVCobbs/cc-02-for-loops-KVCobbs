d050540e517207d503635c6920dbfbd431c5aec7
KVCobbs <kenyacobbs9001@gmail.com>
2021-09-29 14:23:59 +0000

    finished 1-4

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 //1
 function countToTen(){
-  
+  for(let i = 1; i <=10; i++){
+      console.log(i)
+  }
 }
 
 //2
 function countFromOne(){
-  
+  for(let i = 1; i <= 15; i++){
+      console.log(i)
+  }
 }
 
 //3
 function countEveryEven(){
-  
+  for(let i = 0; i <= 40; i++) {
+      if (i === 0) {
+          console.log(i)
+      }
+      else if (i % 2 === 0 ) {
+          console.log(i)
+      }
+  }
 }
 
 //4
 function countEveryOdd(){
-  
+  for(let i = 0; i <= 40; i++){
+      if ( i === 0){
+          console.log(i)
+      }
+      else if (i % 2 != 0) {
+          console.log(i)
+      }
+  }
 }
 
 //5 
@@ -23,3 +41,7 @@ function countEvens(){
   
 }
 
+//countToTen()
+//countFromOne()
+//countEveryEven()
+countEveryOdd()
